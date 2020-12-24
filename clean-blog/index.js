8648fe33c23a0bf572a834371776f0cd381b5fc5
Tyson <tysonjeffreys@gmail.com>
2020-12-23 18:06:33 -0700

    mongoose update

--- clean-blog/index.js
+++ clean-blog/index.js
@@ -5,16 +5,32 @@ const ejs = require('ejs')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
+const BlogPost = require('./models/Blogpost.js')
 mongoose.connect('mongodb://localhost/my_database', {useNewURLParser:true})
+
 
 app.listen(4000, () => {
     console.log('App listening in port 4000')
 })
 
+
+/*
 app.get('/', (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/index.html'))
     res.render('index')
+})
+*/
+app.get('/', async (req,res) => {
+    const blogposts = await BlogPost.find({})
+    //BlogPost.find() returns and array of objects from the database
+    console.log(blogposts)
+    res.render('index', {
+        blogposts
+    });
 })
 
 app.get('/about', (req, res) => {
@@ -32,4 +48,29 @@ app.get('/post', (req, res) => {
     res.render('post')
 })
 
-//test push
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+
+/*
+app.post('/posts/store', (req,res) => {
+    console.log(req.body)
+    BlogPost.create(req.body, (error,blogpost) => {
+        //console.log(blogpost)
+        res.redirect('/')
+    })
+})
+*/
+
+//same function as above but with async/await
+app.post('/posts/store', async (req,res) => {
+    await BlogPost.create(req.body)
+        res.redirect('/')
+    })
+
+app.get('/', async (req,res) => {
+    const blogposts = await BlogPost.find({})
+    res.render('index', {
+        blogposts: blogposts
+    });
+})
