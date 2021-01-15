ab2902df1db920aa722f6cace07eca7bb3a15b7d
Tyson <tysonjeffreys@gmail.com>
2021-01-14 19:12:29 -0700

    Added WYSIWYG editor

--- clean-blog/controllers/newPost.js
+++ clean-blog/controllers/newPost.js
@@ -1,6 +1,9 @@
 module.exports = (req,res) =>{
     if(req.session.userId) {
-        return res.render("create");
+        return res.render("create", {
+            createPost: true
+        });
+
     }
     else {res.redirect('/auth/login')}
 }
